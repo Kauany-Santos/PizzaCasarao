a8be26961a378e7525996bdafe872d5454029ed5
LuisSantos-LS <165852032+Kauany-Santos@users.noreply.github.com>
2024-11-17 14:10:14 -0300

    1.9
    coloquei carrosel e mudei alguns botoes

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,32 @@ menuToggle.addEventListener('click', () => {
     show = !show;
     
 });
+
+//Carrosel dos Destaques//
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+//Setas do Carrosel//
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+//Troca de Imagem//
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("carrosel");
+
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+    
+}
+
